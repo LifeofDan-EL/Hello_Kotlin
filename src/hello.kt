6823353d1c3b1139e3ef4906e9aea6704ae40cd6
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-04-14 23:26:10 +0100

    Initial Commit

---
+++ src/hello.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+
+    // String Concatenation Template
+   var fish1  = "trout"
+   var fish2 = "haddock"
+    var fish3 = "snappec"
+    println("I like to eat $fish1 and $fish2 but not $fish3" )
+
+   // If you start with 2 fish, and they breed twice, producing 71 offspring the first time,
+    // and 223 the second time, and then 13 fishes are swallowed by hungry moray eel, how many fishes do you have left?
+    //How many aquariums will you be needing if you can put 30 fish per aquarium
+   var result = 2.plus(71).plus(233).minus(13).div(30).plus(1)
+    println(result)
+
+    //Create a String variable rainbowColour, set its colour value, then change it
+    var rainbowColour = "red"
+    rainbowColour = "blue"
+
+    //Create a String blackColour whose value cannot be changed once assigned. Try changing it anyways
+    val blackColour = "black"
+
+    print(rainbowColour)
+
+}
