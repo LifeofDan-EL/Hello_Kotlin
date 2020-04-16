96068550c7196381056d6b1371c8be0c4bcb88bb
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-04-16 13:35:39 +0100

    Initial Commit

---
+++ src/FortuneCookie.kt
@@ -0,0 +1,31 @@
+
+
+fun main (args: Array<String>){
+    println("\nYour fortune is: ${getFortuneCookie()}")
+
+    var fortune: String
+
+    for (i in 1..10){
+        fortune  = getFortuneCookie()
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
+    }
+
+    getFortuneCookie()
+}
+
+fun getFortuneCookie(): String {
+    val fortunes = listOf("You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well",
+        "Today is a good day for exercising restraint",
+        "Take it easy and enjoy life",
+        "Treasure your friends because they are your greatest fortune")
+
+    print("\nEnter your birthday: ")
+
+    var birthday = readLine()?.toIntOrNull() ?: 1
+
+    return fortunes[birthday.rem(fortunes.size)]
+}
