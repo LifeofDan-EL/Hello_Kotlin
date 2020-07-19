ba2ded802225912be86b1689bff4c4ec30fbae94
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-07-19 23:45:56 +0100

    Chapter Two Completed

--- src/HeadFirst/codeMagnetTwo.kt
+++ src/HeadFirst/codeMagnetTwo.kt
@@ -20,4 +20,28 @@ fun main(){
         println("Fruit = ${fruit[y]}")
         x += 1
     }
+
+work()
+}
+
+// A short Kotlin program is listed below. When the line
+// Do stuff is reached, some objects and variables have been created.
+// Your task is to determine which of the variables refer to which objects by the time the
+// Do stuff line is reached. Some objects may be referred to more than once.
+fun work (){
+    val x = arrayOf(0, 1, 2, 3, 4)
+    x[3] = x[2]
+    x[4] = x[0]
+    x[2] = x[1]
+    x[1] = x[0]
+    x[0] = x[1]
+    x[4] = x[3]
+    x[3] = x[2]
+    x[2] = x[4]
+
+    var y = 0
+    while (y < 6){
+        println("${x[y]}")
+        y += 1
+    }
 }
